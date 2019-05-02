25c8bbd331cf6b00605460b2b77d9e80af95933c
TsungKaiHo <50210177+TsungKaiHo@users.noreply.github.com>
2019-05-03 00:03:03 +0800

    Add files via upload

---
+++ Home Work9.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Home Work 9
+//
+//  Created by Ho Tsung Kai on 2019/3/27.
+//  Copyright © 2019 Ho Tsung Kai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    int i,i2;
+    for(i=1;i<=9;i++)
+    for(i2=1;i2<=9;i2++)
+        printf("%d * %d\n",i,i2);
+    return 0;
+}
